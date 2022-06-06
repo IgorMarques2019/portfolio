bf5302f438dfc80438e6576019bb7bcf6b021bcf
Igor Marques <igorclash2345@gmail.com>
2022-06-06 20:48:53 -0300

    blabla

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+const iconMenu = document.querySelector('.menu-toggle')
+
+// Quando tiver na largura mobile ativa o icone
+if (window.matchMedia('(max-width:800px').matches) {
+  iconMenu.setAttribute('data-menuToggle', "ativo")
+} else {
+  iconMenu.setAttribute('data-menuToggle', "inativo")
+}
+
+
+let clickCount = 0;
+
+function toggleMenu() {
+  clickCount++;
+  console.log(clickCount)
+  const menuAtive = document.querySelector("header")
+  if (clickCount == 1) {
+    iconMenu.setAttribute('src', "./imgs/icons/menu-open.png")
+    menuAtive.setAttribute("data-menu", 'ativo')
+  } else if (clickCount >= 2) {
+    clickCount = 0;
+    iconMenu.setAttribute('src', "./imgs/icons/menu-hidden.png")
+    menuAtive.setAttribute("data-menu", 'inativo')
+  }
+}
